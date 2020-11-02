595c0f8474d7ae36333d55b08f0748456cc59d3a
TaiNK <yt07071999@gmail.com>
2020-11-02 23:28:18 +0700

    xxx

--- app/src/main/java/com/example/app_music/ui/home/search/SearchFragment.java
+++ app/src/main/java/com/example/app_music/ui/home/search/SearchFragment.java
@@ -11,12 +11,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.app_music.R;
+import com.example.app_music.domain.Song;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchFragment extends Fragment {
 
     private SearchViewModel mViewModel;
+    private DatabaseReference reference;
+    List<Song> songList;
+    private Button button;
 
     public static SearchFragment newInstance() {
         return new SearchFragment();
@@ -25,8 +38,36 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.search_fragment, container, false);
+        songList = new ArrayList<>();
+
+        View root = inflater.inflate(R.layout.search_fragment, container, false);
+        button = root.findViewById(R.id.btn_temp);
+        reference = FirebaseDatabase.getInstance().getReference("xyz");
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                            Song song = dataSnapshot.getValue(Song.class);
+                            songList.add(song);
+
+                        }
+                        System.out.println(songList);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
+        return root;
     }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
